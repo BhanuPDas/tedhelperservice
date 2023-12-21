86848e127a74d384c6a4e3ba3195ef31423facf2
BhanuPDas <bhanu.das001@stud.fh-dortmund.de>
2023-12-21 19:41:30 +0100

    Adding changes for ted Helper service api

--- src/main/java/com/rune/main/ServletInitializer.java
+++ src/main/java/com/rune/main/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.rune;
+package com.rune.main;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
