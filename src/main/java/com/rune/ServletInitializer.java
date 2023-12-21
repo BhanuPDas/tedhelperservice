f1683c9f3960cc0f56bd04190503221909fdea0e
BhanuPDas <bhanu.das001@stud.fh-dortmund.de>
2023-12-21 01:27:10 +0100

    Adding changes for ted helper service

---
+++ src/main/java/com/rune/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.rune;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TedHelperServiceApplication.class);
+	}
+
+}
---
+++ src/main/java/com/rune/TedHelperServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TedHelperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TedHelperServiceApplication.class, args);
+	}
+
+}
