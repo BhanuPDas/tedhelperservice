0c6210d5361bbdbf30124cee2198299f1256422c
BhanuPDas <bhanu.das001@stud.fh-dortmund.de>
2023-12-27 15:44:10 +0100

    Adding changes for TED Helper Service

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 <br>This service updates travel time, break time and work time data on TED. </br>
+<p>
+<br>This is a springboot project. To build this project-</br>
+<br>1)Install Maven</br>
+<br>Run the following commands to build the jar</br>
+<br>2)mvn clean compile package</br>
+</p>
--- src/main/java/com/rune/main/TedHelperServiceApplication.java
+++ src/main/java/com/rune/main/TedHelperServiceApplication.java
@@ -2,7 +2,9 @@ package com.rune.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("com.rune")
@@ -10,6 +12,11 @@ public class TedHelperServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TedHelperServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate createRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
